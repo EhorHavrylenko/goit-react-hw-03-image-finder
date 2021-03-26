c7563b3c9fda250919cebf4b3cd9fd23dc2140a4
Egor Bass <egorbass@MacBook-Pro-Egor.local>
2021-03-26 16:21:40 +0200

    fix axios

--- src/services/fetchImages.js
+++ src/services/fetchImages.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '19920161-2470b7440ca60025ed5778af0';
+const BASE_URL = 'https://pixabay.com/api/';
 
 axios.defaults.baseURL = BASE_URL;
+
 axios.defaults.params = {
 	key: API_KEY,
 	image_type: 'photo',
@@ -11,12 +12,22 @@ axios.defaults.params = {
 	per_page: 12
 };
 
-const fetchImages = async ( q , page ) => {
+// const fetchImages = async ( q , page ) => {
 
-	const images = await axios.get(`https://pixabay.com/api/?q=${q}&page=${page}&key=19920161-2470b7440ca60025ed5778af0&image_type=photo&orientation=horizontal&per_page=12`
-		)
-		
-	return images;
+// 	const images = await axios.get(`https://pixabay.com/api/?q=${q}&page=${page}&key=19920161-2470b7440ca60025ed5778af0&image_type=photo&orientation=horizontal&per_page=12`
+// 		)
+
+// 	return images;
+// };
+
+const fetchImages = async ( q, page ) => {
+	try {
+		const images = await axios.get('', { params: { q, page } });
+		return images;
+	} catch (error) {
+		console.log('error', { error });
+		return [];
+	}
 };
 
 export default fetchImages;
